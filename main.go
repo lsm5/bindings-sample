bc029ed4ea0f0604a9376821d19f8bd7db91a3f8
Lokesh Mandvekar <lsm5@fedoraproject.org>
2020-07-15 05:40:32 -0400

    pull image and add error checks
    Signed-off-by: Lokesh Mandvekar <lsm5@fedoraproject.org>

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 
 	"github.com/containers/libpod/v2/pkg/bindings"
 	"github.com/containers/libpod/v2/pkg/bindings/containers"
+	"github.com/containers/libpod/v2/pkg/bindings/images"
+	"github.com/containers/libpod/v2/pkg/domain/entities"
 	"github.com/containers/libpod/v2/pkg/specgen"
 )
 
@@ -36,28 +38,52 @@ func main() {
 	b := newBinding()
 	err := b.NewConnection()
 	if err != nil {
-		fmt.Printf("%s\n", err)
+		fmt.Println(err)
 		return
+	}
+
+	// Pull image
+	_, err = images.Pull(b.conn, rawImage, entities.ImagePullOptions{})
+	if err != nil {
+		fmt.Println(err)
 	}
 
 	// Container create
 	s := specgen.NewSpecGenerator(rawImage, false)
 	s.Terminal = true
 	r, err := containers.CreateWithSpec(b.conn, s)
+	if err != nil {
+		fmt.Println(err)
+	}
 	// Container start
 	err = containers.Start(b.conn, r.ID, nil)
+	if err != nil {
+		fmt.Println(err)
+	}
 
 	// Container inspect
 	ctrData, err := containers.Inspect(b.conn, r.ID, nil)
+	if err != nil {
+		fmt.Println(err)
+	}
 	fmt.Printf("Container source image is %s\n", ctrData.ImageName)
 	fmt.Printf("Container running status is %s\n", ctrData.State.Status)
 
 	// Container pause
 	//crun failure
-	//err = containers.Pause(b.conn, r.ID)
+	err = containers.Pause(b.conn, r.ID)
+	if err != nil {
+		fmt.Println(err)
+	}
 
 	// Container stop
 	err = containers.Stop(b.conn, r.ID, nil)
+	if err != nil {
+		fmt.Println(err)
+	}
 	ctrData, err = containers.Inspect(b.conn, r.ID, nil)
+	if err != nil {
+		fmt.Println(err)
+	}
 	fmt.Printf("Container running status is now %s\n", ctrData.State.Status)
 }
