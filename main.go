a8bb672a44dca47b247b66c8d1f0d8057ba76171
Lokesh Mandvekar <lsm5@fedoraproject.org>
2020-07-20 10:30:55 -0400

    update as per comments
    Signed-off-by: Lokesh Mandvekar <lsm5@fedoraproject.org>

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"os"
 
 	"github.com/containers/libpod/v2/libpod/define"
 	"github.com/containers/libpod/v2/pkg/bindings"
@@ -12,82 +13,77 @@ import (
 	"github.com/containers/libpod/v2/pkg/specgen"
 )
 
-type binding struct {
-	sock string
-	conn context.Context
-}
-
-func (b *binding) NewConnection() error {
-	connText, err := bindings.NewConnection(context.Background(), b.sock)
-	if err != nil {
-		return err
-	}
-	b.conn = connText
-	return nil
-}
-
-func newBinding() *binding {
-	b := binding{
-		sock: "unix:///run/user/1000/podman/podman.sock",
-	}
-	return &b
-}
-
 func main() {
-	rawImage := "registry.fedoraproject.org/fedora:latest"
-	running := define.ContainerStateRunning
 	fmt.Println("Welcome to Go bindings tutorial")
-	b := newBinding()
-	err := b.NewConnection()
+
+	// Get Podman socket location
+	sock_dir := os.Getenv("XDG_RUNTIME_DIR")
+	socket := "unix://" + sock_dir + "/podman/podman.sock"
+
+	// Connect to Podman socket
+	conn, err := bindings.NewConnection(context.Background(), socket)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
 	// Pull image
+	rawImage := "registry.fedoraproject.org/fedora:latest"
 	fmt.Println("Pulling image...")
-	_, err = images.Pull(b.conn, rawImage, entities.ImagePullOptions{})
+	_, err = images.Pull(conn, rawImage, entities.ImagePullOptions{})
 	if err != nil {
 		fmt.Println(err)
+		return
 	}
+
+	// TODO: Insert Image List code here
 
 	// Container create
 	s := specgen.NewSpecGenerator(rawImage, false)
 	s.Terminal = true
-	r, err := containers.CreateWithSpec(b.conn, s)
+	r, err := containers.CreateWithSpec(conn, s)
 	if err != nil {
 		fmt.Println(err)
+		return
 	}
+
 	// Container start
 	fmt.Println("Starting Fedora container...")
-	err = containers.Start(b.conn, r.ID, nil)
+	err = containers.Start(conn, r.ID, nil)
 	if err != nil {
 		fmt.Println(err)
+		return
 	}
 
 	// Container inspect
-	ctrData, err := containers.Inspect(b.conn, r.ID, nil)
+	ctrData, err := containers.Inspect(conn, r.ID, nil)
 	if err != nil {
 		fmt.Println(err)
+		return
 	}
 	fmt.Printf("Container uses image %s\n", ctrData.ImageName)
 	fmt.Printf("Container running status is %s\n", ctrData.State.Status)
 
-	_, err = containers.Wait(b.conn, r.ID, &running)
+	running := define.ContainerStateRunning
+	_, err = containers.Wait(conn, r.ID, &running)
 	if err != nil {
 		fmt.Println(err)
+		return
 	}
 
 	// Container stop
 	fmt.Println("Stopping the container...")
-	err = containers.Stop(b.conn, r.ID, nil)
+	err = containers.Stop(conn, r.ID, nil)
 	if err != nil {
 		fmt.Println(err)
+		return
 	}
-	ctrData, err = containers.Inspect(b.conn, r.ID, nil)
+	ctrData, err = containers.Inspect(conn, r.ID, nil)
 	if err != nil {
 		fmt.Println(err)
+		return
 	}
 	fmt.Printf("Container running status is now %s\n", ctrData.State.Status)
+	return
 
 }
